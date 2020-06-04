16beeb2cf574ea0651b5d90a08a9710583ae8e0a
SalemJamel <salemjamalfsg@gmail.com>
2020-06-04 15:01:50 +0100

    hhh

---
+++ GESTION DE LOCATION DES VOITURES/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GESTION_DE_LOCATION_DES_VOITURES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
